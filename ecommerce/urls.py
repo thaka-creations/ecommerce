9340944352930e22271877b7e1eb98ec1684d611
321 <321@dig.jnam>
2021-12-11 12:29:03 +0300

    added api documentation

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,12 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
-schema_view = get_schema_view(title='Ecommerce API')
+schema_view = get_schema_view(
+        openapi.Info(
+            title='Ecommerce system',
+            default_version='v1',
+            description='django ecommerce platform',
+        ),
+        public=True,
+        permission_classes = (permissions.AllowAny,),
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,4 +43,8 @@ urlpatterns = [
     #routes for simple jwt
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    #api documentation
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='swagger-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-swagger'),
 ]
